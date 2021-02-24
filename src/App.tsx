2127baad52f8a17eed9382458f6cc5f6bbf0d894
joaosiles <joaosiles@gmail.com>
2021-02-24 17:03:00 -0300

    Conceitos de Componentes, Propriedades e Estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { Button } from './components/Button'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <Button color='green'> 
+        Botão 1 
+      </Button>
+      <Button color='blue'> 
+        Botão 2 
+      </Button>
+    </div>
   );
 }
 
